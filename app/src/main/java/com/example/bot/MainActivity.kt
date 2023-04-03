dfa6089694cc5e155647a44b4d0c5ed53b194f72
Snigdha Reddy Pulim <snigdhareddypulim@crc-dot1x-nat-10-239-100-193.bu.edu>
2023-04-03 05:33:52 -0400

    first commit

---
+++ app/src/main/java/com/example/bot/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.bot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bot.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
