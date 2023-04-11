4e2d8f531240aabe91c3c036ff4bd9c4a8a8ee29
Chen Yang <68773903+MichaelYang0625@users.noreply.github.com>
2023-04-11 18:35:15 -0400

    added the recycler view and a basic fragment as items for testing

--- app/src/main/java/com/example/bot/Event.kt
+++ app/src/main/java/com/example/bot/Event.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,13 +14,26 @@ import android.view.ViewGroup
  * Use the [Event.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Event : Fragment() {
+//class Event : Fragment() {
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_event, container, false)
+//    }
+//}
+class Event(private val event: Event) : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_event, container, false)
+//        view.findViewById<TextView>(R.id.titleTextView).text = event.title
+//        view.findViewById<TextView>(R.id.dateTextView).text = event.date
+        return view
+    }
+
+    companion object {
+        fun newInstance(event: Event) = Event(event)
     }
 }
---
+++ app/src/main/java/com/example/bot/Item.kt
@@ -0,0 +1,3 @@
+package com.example.bot
+
+data class Item(val name: String, val description: String)
