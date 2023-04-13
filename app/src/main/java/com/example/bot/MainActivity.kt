7137416129d95ff47848fcc6febb31339360a9dc
Abhishek <Abhishek@Shaq-Malarkey.local>
2023-04-13 00:57:25 -0400

    file replaced

--- app/src/main/java/com/example/bot/MainActivity.kt
+++ app/src/main/java/com/example/bot/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.example.bot
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.bot.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var eventDataList=listOf("App Dev","OOPS","Software Principles")
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.forEventsActivity.setOnClickListener {
+            //val intent = Intent(this, )
+            //startActivity(intent)
+        }
+        binding.forLogin.setOnClickListener {
+            //val intent = Intent(this, )
+            //startActivity(intent)
+        }
     }
 }
