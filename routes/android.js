83c8b68473fdb13ba9fd690e2a4092c95ccd6867
Abhishek <Abhishek@Shaq-Malarkey.local>
2023-04-23 12:36:43 -0400

    Backend changes

---
+++ routes/android.js
@@ -0,0 +1,26 @@
+var express = require('express');
+const axios = require('axios')
+const fs = require('fs/promises');
+const cs = require('./client_secret.json')
+
+var router = express.Router();
+
+/* GET home page. */
+router.get('/get-token', function(req, res, next) {
+    const id_token = req.query.token
+    cs['code'] = id_token
+    axios.post('https://accounts.google.com/o/oauth2/token', {
+        'code': cs.code,
+        'client_id': cs.web.client_id,
+        'client_secret': cs.web.client_secret,
+        'redirect_uri': 'http://localhost:3000',
+        'grant_type': 'authorization_code',
+    }).then(result => {
+        console.log(result)
+    }).catch(err => {
+        console.log(err)
+    })
+    res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
