b46c00a14ed7e041868f2553c71502e8d7332f9b
Abhishek <Abhishek@Shaq-Malarkey.local>
2023-04-23 14:27:11 -0400

    API Completed to return tokens

--- routes/android.js
+++ routes/android.js
@@ -16,11 +16,18 @@ router.get('/get-token', function(req, res, next) {
         'redirect_uri': 'http://localhost:3000',
         'grant_type': 'authorization_code',
     }).then(result => {
-        console.log(result)
+        // console.log(result.data)
+        return res.send({
+            acces_token: result.data.acces_token,
+            scope: result.data.scope,
+            expires_in: result.data.expires_in,
+            token_type: result.data.token_type,
+            id_token: result.data.id_token
+        })
     }).catch(err => {
         console.log(err)
     })
-    res.render('index', { title: 'Express' });
+    // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
