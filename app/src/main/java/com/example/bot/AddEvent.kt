0af1dc553b8466342e5c99fffd9682953be3be38
Snigdha Reddy Pulim <snigdhareddypulim@crc-dot1x-nat-10-239-100-193.bu.edu>
2023-05-03 23:23:59 -0400

    add_event

--- app/src/main/java/com/example/bot/AddEvent.kt
+++ app/src/main/java/com/example/bot/AddEvent.kt
@@ -5,22 +5,29 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import androidx.compose.material3.Button
+import com.example.bot.databinding.ActivityMainBinding
 import com.google.android.gms.common.api.Status
 import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.model.Place
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import com.example.bot.databinding.AddEventsBinding
 
 class AddEvent : AppCompatActivity() {
-
+    private lateinit var binding: AddEventsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.add_events)
+        binding = AddEventsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val ai: ApplicationInfo = applicationContext.packageManager
             .getApplicationInfo(applicationContext.packageName, PackageManager.GET_META_DATA)
         val value = ai.metaData["api_key"]
         val apiKey = value.toString()
+
+        binding.back.setOnClickListener { finish() }
 
         if (!Places.isInitialized()) {
             Places.initialize(applicationContext, "AIzaSyAmjj4km9mc04VEvtj3mqVEYH6L7kc2vks")
