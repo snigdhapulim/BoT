dc020881d3dd90c3dda0a75f346c5985be29bee2
Snigdha Reddy Pulim <snigdhareddypulim@crc-dot1x-nat-10-239-100-193.bu.edu>
2023-05-09 11:22:22 -0400

    updating

--- app/src/main/java/com/example/bot/MapRouteAdapter.kt
+++ app/src/main/java/com/example/bot/MapRouteAdapter.kt
@@ -3,42 +3,42 @@ package com.example.bot
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.maps.model.DirectionsRoute
 import okhttp3.Route
 
 class MapRouteAdapter(private val routes: Array<DirectionsRoute>) : RecyclerView.Adapter<MapRouteAdapter.ViewHolder>() {
+
+    private var selectedPosition = RecyclerView.NO_POSITION
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        // inflates the card_view_design view
-        // that is used to hold list item
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.routes_list_item, parent, false)
-
         return ViewHolder(view)
     }
 
-    // binds the list items to a view
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = routes[position]
+        holder.nameTextView.text = "Route " + (position+1)
+        holder.durationTextView.text = "Distance: ${item.legs.sumOf { it.distance.inMeters } / 1000.0} km \nDuration: ${item.legs.sumOf { it.duration.inSeconds } / 60} mins"
 
-        val ItemsViewModel = routes[position]
-        holder.nameTextView.text = "Route " + position
-        holder.durationTextView.text = "Distance: ${ItemsViewModel.legs.sumOf { it.distance.inMeters } / 1000.0} km \nDuration: ${ItemsViewModel.legs.sumOf { it.duration.inSeconds } / 60} mins"
+        holder.radioButton.isChecked = position == selectedPosition
 
-
-        // sets the text to the textview from our itemHolder class
-        //holder.textView.text = ItemsViewModel.text
-
+        holder.itemView.setOnClickListener {
+            selectedPosition = holder.adapterPosition
+            notifyDataSetChanged()
+        }
     }
 
-    // return the number of the items in the list
     override fun getItemCount(): Int {
         return routes.size
     }
 
-    // Holds the views for adding it to image and text
-    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var nameTextView: TextView = itemView.findViewById(R.id.text_view_route_name)
         var durationTextView: TextView = itemView.findViewById(R.id.text_view_route_duration)
+        var radioButton: RadioButton = itemView.findViewById(R.id.radio_button_route)
     }
 }
